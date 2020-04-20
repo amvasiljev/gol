8ea960815d6a0d626c1fd38ba75108968c1337bf
Bikeoldstar Alex <Alex@amosam.odlo.ru>
2020-04-20 20:40:24 +0300

    src:

--- src/js/common.js
+++ src/js/common.js
@@ -60,7 +60,32 @@ $('.slider__box_main').slick({
 })
 
 
+$('.shedule__select').niceSelect();
 
+$('.schedule__date_choice').datepicker();
+
+jQuery(function ($) {
+  $.datepicker.regional['ru'] = {
+      closeText: 'Закрыть',
+      prevText: '<Пред',
+      nextText: 'След>',
+      currentText: 'Сегодня',
+      monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+          'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+          'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+      dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+      dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      weekHeader: 'Нед',
+      dateFormat: 'dd.mm.yy',
+      firstDay: 1,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''
+  };   
+      $.datepicker.setDefaults($.datepicker.regional['ru']);
+  });
 
 // // inputs
 // $(document).on('focus', 'input[type="text"]', function () {
